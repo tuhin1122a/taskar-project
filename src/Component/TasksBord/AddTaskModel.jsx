078e137e795ce45c663f1660a3c133d247871e00
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2024-10-11 13:18:00 +0600

    tuhin

---
+++ src/Component/TasksBord/AddTaskModel.jsx
@@ -0,0 +1,103 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+export default function AddTaskModel({ onSave }) {
+  const [createTask, setCreateTask] = useState({
+    id: crypto.randomUUID(),
+    title: "",
+    description: "",
+    tags: [],
+    priority: "",
+    isFavorite: false,
+  });
+  const handleChange = (event) => {
+    const name = event.target.name;
+    let value = event.target.value;
+    if (name === "tags") {
+      value = value.split(",");
+    }
+    setCreateTask({
+      ...createTask,
+      [name]: value,
+    });
+  };
+  return (
+    <>
+      <div className="bg-black bg-opacity-75 h-full w-full z-10 top-0 left-0 absolute"></div>
+      <form className="mx-auto my-10 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11 z-10 absolute top-48 left-1/3">
+        <h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+          Add New Task
+        </h2>
+
+        <div className="space-y-9 text-white lg:space-y-10">
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="title">Title</label>
+            <input
+              className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+              type="text"
+              name="title"
+              value={createTask.title}
+              onChange={handleChange}
+              id="title"
+              required
+            />
+          </div>
+
+          <div className="space-y-2 lg:space-y-3">
+            <label htmlFor="description">Description</label>
+            <textarea
+              className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+              type="text"
+              name="description"
+              id="description"
+              value={createTask.description}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+
+          <div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="tags">Tags</label>
+              <input
+                className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+                type="text"
+                name="tags"
+                id="tags"
+                value={createTask.tags}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="space-y-2 lg:space-y-3">
+              <label htmlFor="priority">Priority</label>
+              <select
+                className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+                name="priority"
+                id="priority"
+                value={createTask.priority}
+                onChange={handleChange}
+                required
+              >
+                <option value="">Select Priority</option>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-16 flex justify-center lg:mt-20">
+          <button
+            type="submit"
+            className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+            onClick={() => onSave(createTask)}
+          >
+            Save Task
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
--- src/Component/TasksBord/TasksBord.jsx
+++ src/Component/TasksBord/TasksBord.jsx
@@ -1,13 +1,13 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
 import TasksList from "../TasksList";
+import AddTaskModel from "./AddTaskModel";
 import SearchBox from "./SearchTask";
 import TasksAction from "./TasksAction";
 
 export default function TasksBord() {
   const defaultTask = {
     id: crypto.randomUUID(),
-    title: "API",
+    title: "React native",
     description:
       "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently",
     tags: ["web", "react", "javaScript"],
@@ -15,15 +15,23 @@ export default function TasksBord() {
     isFavorite: true,
   };
   const [tasks, setTasks] = useState([defaultTask]);
+  const [showAddTask, setShowAddTask] = useState(false);
+  function handleAddTask(task) {
+    console.log("tuhin", task);
+    setTasks([...tasks, task]);
+    setShowAddTask(false);
+  }
+
   return (
     <section className="mb-20" id="tasks">
+      {showAddTask && <AddTaskModel onSave={handleAddTask} />}
       <div className="container">
         <div className="p-2 flex justify-end">
           <SearchBox />
         </div>
 
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TasksAction />
+          <TasksAction onAddClick={() => setShowAddTask(true)} />
           <TasksList tasks={tasks} />
         </div>
       </div>
