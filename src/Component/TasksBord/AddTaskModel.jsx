fc483f2fe5699817db49fc0fd76950a1273955b3
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2024-10-12 02:41:46 +0600

    t

--- src/Component/TasksBord/AddTaskModel.jsx
+++ src/Component/TasksBord/AddTaskModel.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 export default function AddTaskModel({ onSave, taskToUpdate, onCloseClick }) {
   const [createTask, setCreateTask] = useState(
