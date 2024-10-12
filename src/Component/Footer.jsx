cc38606889c2e9c744497950608de4aadc648519
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2024-10-12 11:24:39 +0600

    tuhin

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="py-6 md:py-8">
       <div className="container mx-auto">
         <p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-          Copyright ©2024 | All rights reserved by Learn with Sumit
+          Copyright ©2024 | All rights reserved by Tuhin
         </p>
       </div>
     </footer>
