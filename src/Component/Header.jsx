4dd61d8cb61267b6f5a07bf7becd1286757782fe
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2024-10-12 11:21:16 +0600

    tuhin

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,10 +2,15 @@ import Logo from "../assets/tuhin12.jpg";
 export default function Header() {
   return (
     <nav className="py-6 md:py-8 fixed top-0 w-full !bg-[#191D26] z-50 ">
-      <div className="container mx-auto flex items-center justify-between gap-x-6 rounded-full">
+      <div className="container mx-auto flex items-center justify-start gap-x-6 rounded-full">
         <a href="/">
-          <img className="h-[45px] rounded-full" src={Logo} alt="Lws" />
+          <img
+            className="h-[45px] w-[45px] rounded-full ml-1 border-green-400 border-solid border-4"
+            src={Logo}
+            alt="Lws"
+          />
         </a>
+        <p className="text-3xl">Tuhin</p>
       </div>
     </nav>
   );
